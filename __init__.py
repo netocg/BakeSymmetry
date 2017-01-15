3c9e4106ededa690655913a0705a474815599bc7
Antonio Lisboa M. Neto <netocg.fx@gmail.com>
2017-01-15 16:31:22 -0300

    setShortcut('Ctrl+Alt+S') and reload function
    setShortcut('Ctrl+Alt+S') which also reload the symmetry.py file in case you are authoring it while in Mari.

--- __init__.py
+++ __init__.py
@@ -1,4 +1,11 @@
 import BakeSymmetry.BakeSymmetry_v2 as Bake
-Bake.BakeSymmetryTest()
 
-print "INIT BAKE SYMMETRY"
+def starts_SymmetryBake():
+	reload(Bake)
+	#Bake.BakeSymmetry()
+	print "--- INIT BAKE SYMMETRY ---"
+
+actionShowSymmetryBakeUI = mari.actions.create('Show Bake Symmetry UI', "starts_SymmetryBake()")
+actionShowSymmetryBakeUI.setShortcut('Ctrl+Alt+S')
+#mari.menus.addAction(actionShowSymmetryBakeUI, "MainWindow/d&eTools/&Symmetry")
+print "BAKE SYMMETRY IMPORTED..."
